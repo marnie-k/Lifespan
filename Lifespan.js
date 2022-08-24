7d094a572d67bc5600504f104cf1911d702ed88e
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-08-24 13:00:24 -0400

    adding to github

---
+++ Lifespan.js
@@ -0,0 +1,12 @@
+
+
+function timeRemaining(age) {
+    var days = (90 - age) * 365;
+    var weeks = (90 - age) * 52;
+    var months = (90 - age) * 12;
+
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left to live")
+    
+}
+
+timeRemaining(86)
